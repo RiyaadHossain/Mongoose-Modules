7a47fa40c6304e206908ac15a7d0da738a39e5ec
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-05-12 09:26:16 +0600

    create instaance and save data

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { Application, Request, Response } from "express-serve-static-core";
+import { Schema, model } from "mongoose";
 import express from "express";
 import cors from "cors";
-import { Schema, model } from "mongoose";
+
 const app: Application = express();
-const port: number = 5000;
 
 // Middlewares
 app.use(cors());
@@ -11,18 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Health Check
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   // 1. Create Interface
   interface IUser {
-    id: number;
-    role: "student";
     name: {
       firstName: string;
       lastName: string;
     };
+    role: "student";
     dateOfBirth?: string;
     gender: "male" | "female";
-    email?: string;
+    email: string;
     contactNo: string;
     emergencyContactNo: string;
     presentAddress: string;
@@ -31,18 +30,22 @@ app.get("/", (req: Request, res: Response) => {
 
   // 2. Define Schema
   const userSchema = new Schema<IUser>({
-    id: { type: Number, required: true },
-    role: { type: String, required: true },
     name: {
       firstName: { type: String, required: true },
       lastName: { type: String, required: true },
     },
+    role: { type: String, required: true },
     dateOfBirth: { type: String, required: true },
     gender: {
       type: String,
       enum: ["male", "female"],
     },
-    email: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
     contactNo: { type: String, required: true },
     emergencyContactNo: { type: String, required: true },
     presentAddress: { type: String, required: true },
@@ -50,12 +53,25 @@ app.get("/", (req: Request, res: Response) => {
   });
 
   // 3. Creating Model
-  const User = model("User", userSchema);
-});
+  const User = model<IUser>("User", userSchema);
 
-// Server Listening
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  const newUser = new User({
+    name: {
+      firstName: "Riyad",
+      lastName: "Hossain",
+    },
+    role: "student",
+    dateOfBirth: "11 March, 2001",
+    gender: "male",
+    email: "riyad@gmail.com",
+    contactNo: "01703790978",
+    emergencyContactNo: "01703790978",
+    presentAddress: "Bangladesh",
+    permanentAdress: "Bangladesh",
+  });
+
+  const userData = await newUser.save();
+  res.status(200).json(userData);
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import app from "./app";
 import mongoose from "mongoose";
 
+const port: number = 5000;
+
 // Database connection
 async function dbConnection() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/node-mongodb");
+  await mongoose.connect("mongodb://127.0.0.1:27017/practice-mongoose");
 }
 
 dbConnection()
   .then(() => console.log("Database connected Successfully"))
   .catch((err: Error) => console.log(err));
+
+// Server Listening
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
